f617acc6abe122bd1312ffe6b721a009528e9712
mamelend <m.melend@gmail.com>
2015-09-10 00:01:40 -0400

    update to variables-methods ruby doc

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -2,3 +2,23 @@ puts "What is your favorite number?"
 fav_num = gets.chomp
 better_num = fav_num.to_i + 1
 puts better_num.to_s + " is a much bigger and better number than " + fav_num
+
+=begin 
+How do you define a local variable?
+	variable_name = []
+How do you define a method?
+	def method_name(parameters)
+		end
+What is the difference between a local variable and a method?
+	A local variable points to some bit of data. A method is a predefined function that can be called on a particular instance of it's class in order to return some sort of result.
+How do you run a ruby program from the command line?
+	ruby [filename]
+How do you run an RSpec file from the command line?
+	rpsec [rspec file name]
+What was confusing about this material? What made sense?
+This was all pretty straight forward.
+
+
+Assignment 4.3.1: https://github.com/mamelend/phase-0/blob/master/week-4/address/my_solution.rb
+Assignment 4.3.2: https://github.com/mamelend/phase-0/blob/master/week-4/math/my_solution.rb
+=end
