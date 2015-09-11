f91204b011ead999e446ac3164f32e53bc66debd
mamelend <m.melend@gmail.com>
2015-09-11 17:35:55 -0400

    Add assignment 4.7.2

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,53 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [by myself, with: ].
+#Kai Huang
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input: list of numbers
+# Output: sum of the list of numbers
+# Steps to solve the problem.
+# Iterate through the list of numbers
+# Add numbers to each other
+
+
+# 1. total initial solution
+# def total(array_num)
+#   sum = 0
+#   array_num.each do |x|
+#     sum=sum+x
+#   end
+#   sum
+# end
+
+# 3. total refactored solution
+def total(array_num)
+  array_num.inject{|sum,x| sum+x }
+end
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input: list of strings
+# Output: a sentence from the list
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+def sentence_maker(list_of_words)
+
+  abc = list_of_words.join(' ')
+  abc << "."
+  abc.capitalize!
+  abc
+end
+
+
+# 6. sentence_maker refactored solution
+
+
