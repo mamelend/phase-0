5becfc88e96504e6053a5598eed08deb48d2548e
mamelend <m.melend@gmail.com>
2015-10-01 23:22:36 -0400

    group assignment for week 7

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,39 @@
+//Accountability_Group_2 JavaScript Telephone
+
+// My role in the group is:
+//Guillermo - Release 1: Tests to User Stories
+//*** Miguel -  Release 2: User Stories to Pseudocode ***
+
+
+// Here is my part of the challenge:
+/*
+creation a function sum that takes in a list of numbers
+create a variable to hold the sum
+FOR EACH element in the list of numbers
+add (+=) the number to the sum
+return sum
+
+creation a function mean that takes in a list of numbers
+FOR EACH element in the list of numbers
+add (+=) the number to the sum
+RETURN sum / amount of numbers
+
+creation a function median that takes in a list of numbers
+SORT the numbers in the list
+if the amount of numbers in list does not divide evenly by 2, set a count equal to amount of numbers / 2.
+return index at count
+else if the amount of numbers in list divides evenly by 2, return ((index at (sum / 2 - 1) + index at(sum / 2)) / 2)
+
+
+**Write test code for the following strings
+sum tests
+1,2,3,4,5,5,7 (=> outcome should be 27)
+4,4,5,5,6,6,6,7 (=> outcome shoule be 43)
+mean tests
+1,2,3,4,5,5,7 (=> outcome should be 3.857142857142857)
+4,4,5,5,6,6,6,7 (=> outcocme should be is 5.375)
+meadian tests
+1,2,3,4,5,5,7 (=> outcome should be 4)
+4,4,5,5,6,6,6,7 (=> outcome should be 5.5)
+
+*/
